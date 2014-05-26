6e7c2a974308f07b625a05fb1fa43bb59a0dc60c
Tim Rudat <rudat@mgn-computing.de>
2014-05-26 04:10:50 +0200

    Extend calculation
    Add new tests for border cases.
    Extend calculation logic.

--- lib/moments/difference.rb
+++ lib/moments/difference.rb
@@ -36,6 +36,38 @@ module Moments
       months  = @to.month - @from.month
       years   = @to.year - @from.year
 
+      if seconds < 0
+        seconds += 60
+        minutes -= 1
+      end
+
+      if minutes < 0
+        minutes += 60
+        hours   -= 1
+      end
+
+      if hours < 0
+        hours += 24
+        days  -= 1
+      end
+
+      if days < 0
+        previous_month_days = (Time.new(@to.year, @to.month, 1) - 1).day
+
+        if previous_month_days < @from.day
+          days = previous_month_days + days + (@from.day - previous_month_days)
+        else
+          days = previous_month_days + days
+        end
+
+        months -= 1
+      end
+
+      if months < 0
+        months += 12
+        years -= 1
+      end
+
       @diff = {
           years:   years,
           months:  months,
--- spec/lib/moments/difference_spec.rb
+++ spec/lib/moments/difference_spec.rb
@@ -46,12 +46,30 @@ describe Moments::Difference do
 
         Moments::Difference.new(from, to).to_hash.should == expectation
       end
+
+      it '2013-01-31 23:59:59, 2013-02-01 00:00:00' do
+        from = Time.new 2013, 1, 31, 23, 59, 59
+        to   = Time.new 2013, 2, 1, 0, 0, 0
+
+        expectation = { years: 0, months: 0, days: 0, hours: 0, minutes: 0, seconds: 1 }
+
+        Moments::Difference.new(from, to).to_hash.should == expectation
+      end
     end
 
     context 'minutes' do
       it '2013-01-01 00:00:00, 2013-01-01 00:01:01' do
         from = Time.new 2013, 1, 1, 0, 0, 0
         to   = Time.new 2013, 1, 1, 0, 1, 1
+
+        expectation = { years: 0, months: 0, days: 0, hours: 0, minutes: 1, seconds: 1 }
+
+        Moments::Difference.new(from, to).to_hash.should == expectation
+      end
+
+      it '2013-01-31 23:59:59, 2013-02-01 00:01:00' do
+        from = Time.new 2013, 1, 31, 23, 59, 59
+        to   = Time.new 2013, 2, 1, 0, 1, 0
 
         expectation = { years: 0, months: 0, days: 0, hours: 0, minutes: 1, seconds: 1 }
 
@@ -68,12 +86,30 @@ describe Moments::Difference do
 
         Moments::Difference.new(from, to).to_hash.should == expectation
       end
+
+      it '2013-01-31 23:59:59, 2013-02-01 01:01:00' do
+        from = Time.new 2013, 1, 31, 23, 59, 59
+        to   = Time.new 2013, 2, 1, 1, 1, 0
+
+        expectation = { years: 0, months: 0, days: 0, hours: 1, minutes: 1, seconds: 1 }
+
+        Moments::Difference.new(from, to).to_hash.should == expectation
+      end
     end
 
     context 'days' do
       it '2013-01-01, 2013-01-02' do
         from = Time.new 2013, 1, 1
         to   = Time.new 2013, 1, 2
+
+        expectation = { years: 0, months: 0, days: 1, hours: 0, minutes: 0, seconds: 0 }
+
+        Moments::Difference.new(from, to).to_hash.should == expectation
+      end
+
+      it '2013-01-31, 2013-02-01' do
+        from = Time.new 2013, 1, 31
+        to   = Time.new 2013, 2, 1
 
         expectation = { years: 0, months: 0, days: 1, hours: 0, minutes: 0, seconds: 0 }
 
@@ -90,12 +126,30 @@ describe Moments::Difference do
 
         Moments::Difference.new(from, to).to_hash.should == expectation
       end
+
+      it '2013-01-31, 2013-03-01' do
+        from = Time.new 2013, 1, 31
+        to   = Time.new 2013, 3, 1
+
+        expectation = { years: 0, months: 1, days: 1, hours: 0, minutes: 0, seconds: 0 }
+
+        Moments::Difference.new(from, to).to_hash.should == expectation
+      end
     end
 
     context 'years' do
       it '2013-01-01, 2014-01-01' do
         from = Time.new 2013, 1, 1
         to   = Time.new 2014, 2, 2
+
+        expectation = { years: 1, months: 1, days: 1, hours: 0, minutes: 0, seconds: 0 }
+
+        Moments::Difference.new(from, to).to_hash.should == expectation
+      end
+
+      it '2013-01-31, 2014-03-01' do
+        from = Time.new 2013, 1, 31
+        to   = Time.new 2014, 3, 1
 
         expectation = { years: 1, months: 1, days: 1, hours: 0, minutes: 0, seconds: 0 }
 
