e72abccf06666ca42d71dfe8cb51753c9a595a10
Tim Rudat <timrudat@gmail.com>
2014-11-14 14:17:55 +0100

    Update Moments::Difference
    Refactoring precise_difference method

--- lib/moments/difference.rb
+++ lib/moments/difference.rb
@@ -29,55 +29,56 @@ module Moments
     end
 
     def precise_difference
-      seconds = @to.sec - @from.sec
-      minutes = @to.min - @from.min
-      hours   = @to.hour - @from.hour
-      days    = @to.day - @from.day
-      months  = @to.month - @from.month
-      years   = @to.year - @from.year
-
-      if seconds < 0
-        seconds += 60
-        minutes -= 1
-      end
-
-      if minutes < 0
-        minutes += 60
-        hours   -= 1
-      end
-
-      if hours < 0
-        hours += 24
-        days  -= 1
-      end
-
-      if days < 0
-        previous_month_days = (Time.new(@to.year, @to.month, 1) - 1).day
-
-        if previous_month_days < @from.day
-          days = previous_month_days + days + (@from.day - previous_month_days)
-        else
-          days = previous_month_days + days
-        end
-
-        months -= 1
-      end
-
-      if months < 0
-        months += 12
-        years -= 1
-      end
-
       @diff = {
-          years:   years,
-          months:  months,
-          days:    days,
-          hours:   hours,
-          minutes: minutes,
-          seconds: seconds
+        seconds: @to.sec - @from.sec,
+        minutes: @to.min - @from.min,
+        hours: @to.hour - @from.hour,
+        days: @to.day - @from.day,
+        months: @to.month - @from.month,
+        years: @to.year - @from.year,
       }
+
+      if @diff[:seconds] < 0
+        @diff[:seconds] += 60
+        @diff[:minutes] -= 1
+      end
+
+      if @diff[:minutes] < 0
+        @diff[:minutes] += 60
+        @diff[:hours] -= 1
+      end
+
+      if @diff[:hours] < 0
+        @diff[:hours] += 24
+        @diff[:days] -= 1
+      end
+
+      if @diff[:days] < 0
+        previous_month_days = (Time.new(@to.year, @to.month, 1) - 1).day
+        @diff[:days] = precise_previous_month_days(@diff[:days], previous_month_days, @from.day)
+        @diff[:months] -= 1
+      end
+
+      if @diff[:months] < 0
+        @diff[:months] += 12
+        @diff[:years] -= 1
+      end
+
+      @diff
     end
 
     private :precise_difference
+    
+    def precise_previous_month_days(days, previous, from)
+      if previous < from
+        days = previous + days + (from - previous)
+      else
+        days = previous + days
+      end
+      
+      days
+    end
+    
+    private :precise_previous_month_days
   end
 end
