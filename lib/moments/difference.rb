0d566f2cac4272df3c2f339ecd1418fe90b9fda1
Tim Rudat <rudat@mgn-computing.de>
2014-11-14 18:18:59 +0100

    Refactoring
    Fix code duplication

--- lib/moments/difference.rb
+++ lib/moments/difference.rb
@@ -9,15 +9,6 @@ module Moments
         raise ArgumentError.new(message)
       end
 
-      @diff = {
-        seconds: @to.sec - @from.sec,
-        minutes: @to.min - @from.min,
-        hours: @to.hour - @from.hour,
-        days: @to.day - @from.day,
-        months: @to.month - @from.month,
-        years: @to.year - @from.year,
-      }
-
       precise_difference
     end
 
@@ -38,43 +29,34 @@ module Moments
     end
 
     def precise_difference
-      calculate_seconds
-      calculate_minutes
-      calculate_hours
+      @diff = {
+        seconds: @to.sec - @from.sec,
+        minutes: @to.min - @from.min,
+        hours: @to.hour - @from.hour,
+        days: @to.day - @from.day,
+        months: @to.month - @from.month,
+        years: @to.year - @from.year,
+      }
+
+      calculate :seconds, :minutes
+      calculate :minutes, :hours
+      calculate :hours, :days, 24
       calculate_days
-      calculate_months
+      calculate :months, :years, 12
 
       @diff
     end
 
     private :precise_difference
 
-    def calculate_seconds
-      if @diff[:seconds] < 0
-        @diff[:seconds] += 60
-        @diff[:minutes] -= 1
+    def calculate(attr, subtr, increment = 60)
+      if @diff[attr] < 0
+        @diff[attr] += increment
+        @diff[subtr] -= 1
       end
     end
 
-    private :calculate_seconds
-
-    def calculate_minutes
-      if @diff[:minutes] < 0
-        @diff[:minutes] += 60
-        @diff[:hours] -= 1
-      end
-    end
-
-    private :calculate_minutes
-
-    def calculate_hours
-      if @diff[:hours] < 0
-        @diff[:hours] += 24
-        @diff[:days] -= 1
-      end
-    end
-
-    private :calculate_hours
+    private :calculate
 
     def calculate_days
       if @diff[:days] < 0
@@ -85,15 +67,6 @@ module Moments
     end
 
     private :calculate_days
-
-    def calculate_months
-      if @diff[:months] < 0
-        @diff[:months] += 12
-        @diff[:years] -= 1
-      end
-    end
-
-    private :calculate_months
 
     def precise_previous_month_days(days, previous, from)
       if previous < from
