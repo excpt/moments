7bcfdfb671c42d6ff2a37285d6ef95c1bb3e476d
Tim Rudat <rudat@mgn-computing.de>
2014-11-14 17:57:43 +0100

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
